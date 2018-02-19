4ea2369c9dd6ac1d922d730556a79557217a19ca
Lewis Cook <35107692+LewisBCook@users.noreply.github.com>
2018-02-19 10:13:25 -0500

    Got DevTools.proj (DisableRuleDirectiveProcessor) building under VS 2017.

--- BuildDevTools.bat
+++ BuildDevTools.bat
@@ -5,6 +5,6 @@ SET RootDir=%~dp0.
 :: NUBuild and NUBuildVS need to be built separately, before any other projects, since all of the later projects depend on them.
 MSBuild.exe /nologo "%RootDir%\Tools\NUBuild\NUBuild.sln" %*
 MSBuild.exe /nologo "%RootDir%\Tools\NUBuildVS\NUBuildVS.sln" %*
-REM MSBuild.exe /nologo "%RootDir%\DevTools.proj" %*
+MSBuild.exe /nologo "%RootDir%\DevTools.proj" %*
 
 GOTO:EOF
