4c42f293769a435f197f1314a180a37293b88818
Lewis Cook <35107692+LewisBCook@users.noreply.github.com>
2018-03-05 11:37:45 -0500

    Got Setup to build

--- BuildSetup.bat
+++ BuildSetup.bat
@@ -3,7 +3,22 @@ SETLOCAL
 SET RootDir=%~dp0.
 CALL "%RootDir%\SetupEnvironment.bat" %*
 
-CALL "%RootDir%\BuildHelp.bat" %*
-MSBuild.exe /nologo "%RootDir%\Setup.proj" %*
+:: HelpStudio Lite is not installed anymore with VS SDK
+IF NOT "%TargetVisualStudioMajorMinorVersion%"=="15.0" (
+	CALL "%RootDir%\BuildHelp.bat" %*
+)
+:: If VSWhere was used then we need to pass in the private asssembly location - this is the newer VS
+CALL:SetupFrom%VSInformationSource% %*
 
+GOTO:EOF
+
+:SetupFromRegistry
+MSBuild.exe /nologo "%RootDir%\Setup.proj" %*
+GOTO:EOF
+
+:SetupFromVSWhere
+FOR /f "usebackq tokens=*" %%i IN (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) DO (
+	SET VSInstallDir=%%i
+)
+MSBuild.exe /nologo "%RootDir%\Setup.proj" /p:"ReferencePath=%VSInstallDir%\Common7\IDE\PrivateAssemblies" %*
 GOTO:EOF
