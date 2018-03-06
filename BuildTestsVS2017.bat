0f28eb5d46a17ca05a62c50b9b91d0431551c521
Lewis Cook <35107692+LewisBCook@users.noreply.github.com>
2018-03-06 12:09:00 -0500

    Updated test projects to build with VS 2017

--- BuildTestsVS2017.bat
+++ BuildTestsVS2017.bat
@@ -1,6 +1,5 @@
 @ECHO OFF
 SETLOCAL
-SET RootDir=%~dp0.
 
 IF "%TargetVisualStudioVersion%"=="v8.0" (
 	SET DegradeToolsVersion=/toolsversion:2.0
@@ -11,7 +10,6 @@ IF "%TargetVisualStudioVersion%"=="v8.0" (
 ) ELSE IF "%TargetVisualStudioVersion%"=="v11.0" (
 	SET DegradeToolsVersion=/toolsversion:4.0
 ) ELSE IF "%TargetVisualStudioVersion%"=="v12.0" (
-	SET TargetVisualStudioVersion=v12.0
 	SET DegradeToolsVersion=/toolsversion:12.0
 ) ELSE IF "%TargetVisualStudioVersion%"=="v14.0" (
 	SET TargetVisualStudioVersion=v14.0
@@ -21,11 +19,7 @@ IF "%TargetVisualStudioVersion%"=="v8.0" (
 	SET DegradeToolsVersion=/toolsversion:15.0
 )
 
-CALL "%RootDir%\BuildDevTools.bat" %* /consoleloggerparameters:DisableMPLogging %DegradeToolsVersion%
-
 FOR /f "usebackq tokens=*" %%i IN (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) DO (
 	SET VSInstallDir=%%i
 )
-CALL "%RootDir%\Build.bat" %* /p:"ReferencePath=%VSInstallDir%\Common7\IDE\PrivateAssemblies" /consoleloggerparameters:DisableMPLogging %DegradeToolsVersion%
-
-GOTO:EOF
+CALL "%~dp0BuildTests.bat" %* /p:"ReferencePath=%VSInstallDir%\Common7\IDE\PrivateAssemblies" /consoleloggerparameters:DisableMPLogging %DegradeToolsVersion%
