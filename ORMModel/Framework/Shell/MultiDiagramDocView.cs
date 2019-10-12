129ebe848cb9c3c234b2b77f60c91368563bc860
Lewis Cook <35107692+LewisBCook@users.noreply.github.com>
2019-10-12 11:06:18 -0400

    Pulled in Mat's fix from his repository to fix the DPI Issue https://github.com/ormsolutions/NORMA/commit/90fea37baa472e16dcc9707056c0c0f94c6d159d

--- ORMModel/Framework/Shell/MultiDiagramDocView.cs
+++ ORMModel/Framework/Shell/MultiDiagramDocView.cs
@@ -748,6 +748,58 @@ namespace ORMSolutions.ORMArchitect.Framework.Shell
 				}
 			}
 		}
-		#endregion // IModelingEventSubscriber Implementation
-	}
+        #endregion // IModelingEventSubscriber Implementation
+
+        // HACK: Pulling in a DPI bug fix from Matt's repo: https://github.com/ormsolutions/NORMA/commit/90fea37baa472e16dcc9707056c0c0f94c6d159d
+        // This will need to be removed once Microsoft has fixed the issue
+#if VISUALSTUDIO_16_0
+        private static System.Reflection.MethodInfo getDpiState;
+		private static System.Reflection.MethodInfo forceRecreate;
+
+		/// <summary>
+		/// Verify compatible Dpi settings so the DiagramClientView does
+		/// not disappear.
+		/// </summary>
+		public override VSDiagramView CreateDiagramView()
+		{
+			// On reload, the DiagramClientView and VSDiagramView windows end
+			// up with different Dpi settings. This causes the internal Windowss
+			// SetParent API to return a ERROR_INVALID_STATE result after Windows 10,
+			// release 1703 and later. This effectively orphans the DiagramClientView,
+			// which is the window the diagram shapes draw in, so all of the diagram
+			// windows are blank. This is bad.
+			// UNDONE: MSBUG: There is no way I should have to fix this here. The
+			// DiagramClientView window handle is created much earlier than it
+			// needs to be causing the window to be temporarily 'parked'. Creating the
+			// window for the child DiagramClientView after the VSDiagramView handle is
+			// created would eliminate all of this hacking.
+			VSDiagramView designer = base.CreateDiagramView();
+
+			if (forceRecreate == null)
+			{
+				// The first one is most reliable (part of the public API), the internal less so.
+				forceRecreate = typeof(Control).GetMethod("RecreateHandle", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public);
+				getDpiState = typeof(Control).GetMethod("get_DpiAwarenessContext", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+			}
+
+			if (getDpiState != null)
+			{
+				EventHandler designerHandleCreated = null;
+				designer.HandleCreated += designerHandleCreated = delegate (object sender, EventArgs e)
+				{
+					VSDiagramView view = (VSDiagramView)sender;
+					view.HandleCreated -= designerHandleCreated; // Run once
+					DiagramClientView clientView = view.DiagramClientView;
+					if (clientView.IsHandleCreated &&
+						(int)getDpiState.Invoke(clientView, null) != (int)getDpiState.Invoke(view, null))
+					{
+						forceRecreate.Invoke(clientView, null);
+					}
+				};
+			}
+			return designer;
+		}
+#endif // VISUALSTUDIO_16_0
+        // END HACK
+    }
 }
